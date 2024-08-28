e1a198a65d17de71281113981cd519c12b567e7e
Hillary Onyango <hillaryonyango19@gmail.com>
2024-08-28 13:11:04 +0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import Home from "./Pages/Home/Home";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/Register/Register";
 
 function App() {
-  return <Home />;
+  return <Profile />;
 }
 
 export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Topbar from "../../Components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
 
-function Home() {
-  return <div>This is homepage</div>;
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
-
-export default Home;
